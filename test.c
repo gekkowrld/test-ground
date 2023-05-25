0edd4b0f19b481a3cd1550723136d1f6d64f7e59
Restyled.io <commits@restyled.io>
2023-05-25 22:04:00 +0000

    Restyled by astyle

--- test.c
+++ test.c
@@ -1,10 +1,10 @@
 # include <stdio.h>
 
-int main(){
-printf("Test, for C\n");
-  hello();
+int main() {
+    printf("Test, for C\n");
+    hello();
 }
 
-void hello(void){
-  printf("Hello, Testers\n");
+void hello(void) {
+    printf("Hello, Testers\n");
 }
